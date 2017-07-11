6f45b7b15c0fac740348072f8d92b825c7efe2f7
ipa-mjp <mayank.jitendrakumar.patel@ipa.fraunhofer.de>
2017-07-11 12:55:41 +0200

    compute jacobi accord to type of joint but still error for computing jacobi

--- nmpc_python/src/fk.py
+++ nmpc_python/src/fk.py
@@ -8,7 +8,7 @@ from kdl_parser_py.urdf import treeFromUrdfModel
 from urdf_parser_py.urdf import Robot
 import tf.transformations
 
-def create_kinematic_chain_from_robot_description(base_link="base_link", end_link="gripper", urdf_file=None, robot_description="/robot_description"):
+def create_kinematic_chain_from_robot_description(base_link="arm_base_link", end_link="arm_wrist_3_link", urdf_file=None, robot_description="/robot_description"):
     if urdf_file is None:
         robot = Robot.from_parameter_server(key=robot_description)
     else:
@@ -62,7 +62,7 @@ class KinematicChain(object):
         links_name = []
         links_name = self.urdf_model_.get_chain(self.base_link_, self.end_link_, joints=joints, fixed=fixed)
         return links_name
-
+    
     ##
     # @param Name of joint of which transformation needed
     # @return 4x4 homogeneous transformation
@@ -87,9 +87,47 @@ class KinematicChain(object):
             trans = self.create_homo_matrix(it)
             trans_wrt_origin = np.dot(trans_wrt_origin, trans)
             list_fk.append((it, self.joints_type_[i], trans, trans_wrt_origin))
+            #print trans_wrt_origin
         return list_fk
 
+    # end_pose(On) final pose of end_link wrt to base or fix link
+    def compute_jacobi(self, list_fk, end_pose=None):
+        On = end_pose[:3,3]
+        jacobi_mat = []
 
+        for i in range(0,len(self.joints_name_)):
+            mat_wrt_ori = list_fk[i][3]
+            Zi = mat_wrt_ori[:3,2]
+
+            if self.joints_type_[i] == 'revolute':
+                Oi = mat_wrt_ori[:3,3]
+                jacobi_mat.append((np.cross(Zi, On - Oi), Zi))
+
+            elif self.joints_type_[i] == 'prismatic':
+                jacobi_mat.append((Zi, [0.0, 0.0, 0.0]))
+
+            else:
+                rospy.loginfo("joint type is fixed")
+
+#        # Acoord to ref solution, My Z is wrong so it gives wrong result
+#        for i in range(-1,len(self.joints_name_)):
+#
+#            if i is not -1:
+#                mat_wrt_ori = list_fk[i][3]
+#                Zi = Z0
+#                Z0 = mat_wrt_ori[:3,2]
+#                #Z0 = [0, 1, 0]
+#                Oi = O0
+#                O0 = mat_wrt_ori[:3,3]
+#                #print "zi: ",Zi
+#                #print "Oi: ",Oi
+#                jacobi_mat.append((np.cross(Zi, On - Oi), Zi))
+#
+#            if i is -1:
+#                Z0 = [0 , 0, 1]
+#                O0 = [0, 0 , 0]
+
+        return jacobi_mat
 
 if __name__ == '__main__':
 
@@ -98,7 +136,9 @@ if __name__ == '__main__':
     if not rospy.is_shutdown():
         robot = Robot.from_parameter_server()
         kdl_chain = KinematicChain(robot)
-        pose = kdl_chain.write_to_list()[5]
-
+        pose = kdl_chain.write_to_list()
+        #print "pose2: ",pose[2][3]
+        jacobi_mat = kdl_chain.compute_jacobi(pose, end_pose=pose[5][3])  # means 5 joint element of list and 3 element of that joint)
+        print np.squeeze(np.asarray(jacobi_mat))
     else:
         rospy.logerr("Try to connect ROS")
